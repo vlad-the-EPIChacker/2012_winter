e1ff79d58bf7c1f4456a96ed6bc0f8f185d917f0
Vlad <vladov3000@gmail.com>
2016-08-14 20:34:53 -0700

    a

--- pp.py
+++ pp.py
@@ -2,11 +2,31 @@ import sys
 
 def answer(line):
     line=line.split()
+    final=[]
     for q in range(0, len(line)):
         line[q]=int(line[q])
+    a=line[0]
+    b=line[1]
+    sa=1
+    sb=1
     for n in range(0,1000):
+        if n==0:
+            sa=1
+        else:
+            sa=sa*a
+        sb=1
         for m in range(0, 1000):
-    return line
+            if m==0:
+                sb=1
+            else:
+                sb=sb*b
+            s=sa*sb
+            if s<100000 and s>=10000:
+                final.append(s)
+            elif s>=100000:
+                break
+
+    return final
 
 f=open(sys.argv[1])
 file=f.readlines()
