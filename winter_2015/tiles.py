1e79a2e1b64b6412f53a9a04bb53f821a7e52512
Vlad <vladov3000@gmail.com>
2016-10-01 10:18:37 -0700

    a

---
+++ winter_2015/tiles.py
@@ -0,0 +1,19 @@
+import sys
+
+def math5(x):
+    x=x.split()
+    l=int(x[0])
+    w=int(x[1])
+    a=l*w
+    a=a+a/10
+    if  a%10==0:
+        return a
+    else:
+        return int(a)+1
+
+f=open(sys.argv[1])
+file=f.readlines()
+nfile=int(file[0])+1
+
+for i in range(1, nfile):
+    print math5(file[i])
